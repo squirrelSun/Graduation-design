595d81ed30b799fcbb1c513b96060480e635a525
sunwenyuan <sunwwyy@163.com>
2021-04-21 10:56:21 +0800

    entity

---
+++ crowd07-member-parent/crowd10-member-mysql-provider/src/main/java/com/sust/swy/crowd/CrowdMainClass.java
@@ -0,0 +1,15 @@
+package com.sust.swy.crowd;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.sust.swy.crowd.mapper")
+@SpringBootApplication
+public class CrowdMainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdMainClass.class, args);
+	}
+
+}
